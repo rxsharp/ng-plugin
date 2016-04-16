fd53913d856eb331b64b49065752a348df95b103
Rick G <lacunainc04@gmail.com>
2016-04-16 23:36:52 +0000

    Add link to settings

--- ng-plugin.php
+++ ng-plugin.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       ng-plugin
  * Plugin URI:        rickg.me
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       An angular app in wordpres
  * Version:           1.0.0
  * Author:            Rick G
  * Author URI:        rickg.me
