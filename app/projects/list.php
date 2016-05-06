ea485c89ece36af1ab775c2d4ffd6ae94ce2edc2
Rick G <lacunainc04@gmail.com>
2016-05-06 16:44:12 -0700

    Disable scrollify directive

--- app/projects/list.php
+++ app/projects/list.php
@@ -1,3 +1,6 @@
 <div class="portfolioPage">
-<scrollify></scrollify>
+    <div ng-repeat="post in posts" class="section-class-name">
+   	<h2 ng-bind-html="post.title.rendered | to_trusted"></h2>
+   		<a ui-sref="detail({id: post.id})"> &gt; </a>
+    </div>
 </div>
