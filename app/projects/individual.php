87bf16c1ac1857f72f53ac30960163b5f7562159
Rick G <lacunainc04@gmail.com>
2016-05-10 08:53:01 -0700

    Add background color api functionality

--- app/projects/individual.php
+++ app/projects/individual.php
@@ -1,3 +1,4 @@
-<div class="individualPortfolioPage">
+<div class="individualPortfolioPage" style="background-color: {{post.acf.background_color}};">
     <post-content></post-content>
+    <strong></strong>
 </div>
